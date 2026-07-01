d55c616f60b0b47345d4c82715bf7ee122b23838
Nate Benes <technicaladvisor@bacnetinternational.org>
2026-07-01 09:38:52 -0500

    fix value of FAULT bit in BACnetStatusFlags
    Fixes #2, thanks Matthew!

--- dev/src/java/dev/bscs/bacnet/stack/data/BACnetStatusFlags.java
+++ dev/src/java/dev/bscs/bacnet/stack/data/BACnetStatusFlags.java
@@ -6,7 +6,7 @@ import dev.bscs.bacnet.stack.data.base.BACnetBitString;
 public class BACnetStatusFlags extends BACnetBitString {
     public BACnetStatusFlags()      { super(_LENGTH); }
     public static final int IN_ALARM       = 0;
-    public static final int FAULT          = 2;
+    public static final int FAULT          = 1;
     public static final int OVERRIDDEN     = 2;
     public static final int OUT_OF_SERVICE = 3;
     public static final int _LENGTH        = 4;
